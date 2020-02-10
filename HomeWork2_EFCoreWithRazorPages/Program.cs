d269294540061db4d914e2313677c9a39f43d965
Andrei Koltšenko <andrei.koltsenko@ttu.ee>
2020-02-10 19:45:08 +0200

    TimeLog, Created DbInitializer, updated Program.cs and Drop-Database in PM

--- HomeWork2_EFCoreWithRazorPages/Program.cs
+++ HomeWork2_EFCoreWithRazorPages/Program.cs
@@ -31,7 +31,7 @@ namespace HomeWork2_EFCoreWithRazorPages
                 try
                 {
                     var context = services.GetRequiredService<SchoolContext>();
-                    context.Database.EnsureCreated();
+                    DbInitializer.Initialize(context);
                 }
                 catch (Exception ex)
                 {
