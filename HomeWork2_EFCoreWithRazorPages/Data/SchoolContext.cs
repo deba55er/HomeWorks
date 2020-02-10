613fa65dffe1790ccd89fc5e2204493fc1b68a49
Andrei Koltšenko <andrei.koltsenko@ttu.ee>
2020-02-10 22:53:56 +0200

    Updated Data (DbInitializer and SchoolContext), added entity Enrollment and CourseAssignement, migrated and updated

--- HomeWork2_EFCoreWithRazorPages/Data/SchoolContext.cs
+++ HomeWork2_EFCoreWithRazorPages/Data/SchoolContext.cs
@@ -14,17 +14,27 @@ namespace HomeWork2_EFCoreWithRazorPages.Data
         {
         }
 
-        public DbSet<Student> Students { get; set; }
-        public DbSet<Enrollment> Enrollments { get; set; }
         public DbSet<Course> Courses { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<Instructor> Instructors { get; set; }
+        public DbSet<OfficeAssignment> OfficeAssignments { get; set; }
+        public DbSet<CourseAssignment> CourseAssignments { get; set; }
+
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Course>().ToTable("Course");
             modelBuilder.Entity<Enrollment>().ToTable("Enrollment");
             modelBuilder.Entity<Student>().ToTable("Student");
-        }
+            modelBuilder.Entity<Department>().ToTable("Department");
+            modelBuilder.Entity<Instructor>().ToTable("Instructor");
+            modelBuilder.Entity<OfficeAssignment>().ToTable("OfficeAssignment");
+            modelBuilder.Entity<CourseAssignment>().ToTable("CourseAssignment");
 
-        public DbSet<HomeWork2_EFCoreWithRazorPages.Models.Student> Student { get; set; }
+            modelBuilder.Entity<CourseAssignment>()
+                .HasKey(c => new { c.CourseID, c.InstructorID });
+        }
     }
 }
